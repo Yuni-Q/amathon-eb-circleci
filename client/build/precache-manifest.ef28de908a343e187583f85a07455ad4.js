5ff51299a435a2f5621fa9f5bc299f4ce9c9a123
Yuni-Q <carnotcycle2@naver.com>
2019-08-29 19:42:54 +0900

    first commit

---
+++ client/build/precache-manifest.ef28de908a343e187583f85a07455ad4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df8ef23add08b62eeb7e0d924fb9e8a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be176210b5589d1e55cc",
+    "url": "/static/css/main.9093acdf.chunk.css"
+  },
+  {
+    "revision": "89ec4b68f411cd1148f4",
+    "url": "/static/js/2.12d29ce0.chunk.js"
+  },
+  {
+    "revision": "be176210b5589d1e55cc",
+    "url": "/static/js/main.c5aa2a0e.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  }
+]);
---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './App.css';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todoList: []
+    }
+  }
+  getToDo = async () => {
+    try {
+      const response = await axios.get('/todos');
+      console.log('response data: ', response.data)
+      this.setState({
+        todoList: response.data
+      })
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  render() {
+    console.log(this.state.todolist)
+    const todos = this.state.todoList
+    return (
+      <div className='App'>
+        <h1>Hello Stranger?</h1>
+        <button onClick={this.getToDo}>
+          TODO
+        </button>
+        <div>
+          {todos.map(todo => (
+            <h3 key={todo.id}>
+              제목: {todo.title}<br/>설명: {todo.description}
+            </h3>
+          ))}
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default App;
